604726eda328192c545237a78ff45003868491fe
kaiwoot <kaiwon.sacks@gmail.com>
2024-06-10 15:31:05 -0700

    Stuff I need to commit on my end.

--- src/main/java/koffee/magictoffee/item/ModItems.java
+++ src/main/java/koffee/magictoffee/item/ModItems.java
@@ -3,7 +3,9 @@ package koffee.magictoffee.item;
 import koffee.magictoffee.MagicToffee;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -21,5 +23,7 @@ public class ModItems {
 
     public static void registerModItems() {
         MagicToffee.LOGGER.info("Registering Mod Items For Magic Toffee");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
