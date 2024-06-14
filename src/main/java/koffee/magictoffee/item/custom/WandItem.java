19f37d40ad141162daad1041783c83e4ed2781dc
kaiwoot <kaiwon.sacks@gmail.com>
2024-06-14 15:40:09 -0700

    Changed wand generated to handheld

---
+++ src/main/java/koffee/magictoffee/item/custom/WandItem.java
@@ -0,0 +1,8 @@
+package koffee.magictoffee.item.custom;
+
+import net.minecraft.item.*;
+import net.minecraft.util.ActionResult;
+
+public class WandItem extends Item {
+    public WandItem(Settings settings) { super(settings); }
+}
