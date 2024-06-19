e2df85b1198af8f294af77b24094d3a83be56d33
RedToffee <Dreadnaxela@gmail.com>
2024-06-18 23:05:05 -0600

    Made change playerdata

--- src/main/java/koffee/magictoffee/networking/packet/Change_SpellC2SPacket.java
+++ src/main/java/koffee/magictoffee/networking/packet/Change_SpellC2SPacket.java
@@ -1,6 +1,8 @@
 package koffee.magictoffee.networking.packet;
 
 import koffee.magictoffee.item.custom.WandItem;
+import koffee.magictoffee.util.IEntityDataSaver;
+import koffee.magictoffee.util.SpellData;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
@@ -14,10 +16,13 @@ public class Change_SpellC2SPacket {
                                PacketByteBuf buf, PacketSender responseSender) {
         // This is called when a packet is received by the server from the client
         if (player.getStackInHand(Hand.MAIN_HAND).getItem() instanceof WandItem) {
+
             if (player.isSneaking()) {
-                player.sendMessage(Text.literal("Change spell back"), true);
+                int test = SpellData.addSelected((IEntityDataSaver) player, -1);
+                player.sendMessage(Text.literal("Change spell back" + String.valueOf(test)), true);
             } else {
-                player.sendMessage(Text.literal("Change spell"), true);
+                int test = SpellData.addSelected((IEntityDataSaver) player, 1);
+                player.sendMessage(Text.literal("Change spell" + String.valueOf(test)), true);
             }
         }
     }
