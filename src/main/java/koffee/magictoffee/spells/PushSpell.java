5a5ec65d1e614a85b64a7f64406552d31ea270dd
kaiwoot <kaiwon.sacks@gmail.com>
2024-06-20 12:09:02 -0700

    Pushed push

---
+++ src/main/java/koffee/magictoffee/spells/PushSpell.java
@@ -0,0 +1,36 @@
+package koffee.magictoffee.spells;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+
+public class PushSpell extends Spell{
+
+    public PushSpell() {
+        super.spellID = "magictofee:push";
+        super.displayName = "Push";
+    }
+
+    @Override
+    public void ActionOnUse(PlayerEntity player) {
+        Vec3d targetBlock = getTargetBlock(player);
+        Vec3d playerPos = player.getEyePos();
+        if (targetBlock != null) {
+            Vec3d vector = playerPos.subtract(targetBlock);
+            Vec3d desiredVector = (vector.normalize()).multiply(2);
+            player.setVelocityClient(desiredVector.x, desiredVector.y, desiredVector.z);
+        }
+    }
+
+    private Vec3d getTargetBlock(PlayerEntity player) {
+        HitResult hitResult = player.raycast(10.0D, 0.0F, true);
+        if (hitResult.getType() == HitResult.Type.BLOCK) {
+            BlockHitResult targetBlock = (BlockHitResult) hitResult;
+            return targetBlock.getPos();
+        }
+        else {
+            return null;
+        }
+    }
+}
