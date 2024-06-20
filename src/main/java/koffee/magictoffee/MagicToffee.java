5b623f7206e1bce11179c87c0d8d6541cbda5046
RedToffee <Dreadnaxela@gmail.com>
2024-06-19 19:36:29 -0600

    Player Join Listener

--- src/main/java/koffee/magictoffee/MagicToffee.java
+++ src/main/java/koffee/magictoffee/MagicToffee.java
@@ -28,6 +28,8 @@ public class MagicToffee implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		// Items and Item Groups
 		ModItems.registerModItems();
 		ModItemGroups.registerItemGroups();
 
