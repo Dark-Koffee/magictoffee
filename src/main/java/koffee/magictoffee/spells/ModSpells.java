275e75854477f7086978af5359cfb5f2cce6b36b
RedToffee <Dreadnaxela@gmail.com>
2024-06-19 23:05:24 -0600

    Registered Empty Spell & Properly named Magic Missile

--- src/main/java/koffee/magictoffee/spells/ModSpells.java
+++ src/main/java/koffee/magictoffee/spells/ModSpells.java
@@ -1,13 +1,14 @@
 package koffee.magictoffee.spells;
-
 import koffee.magictoffee.MagicToffee;
 
 public class ModSpells {
 
     // TestSpell
     public static final Spell testSpell = registerSpell(new TestSpell());
+    // Empty Spell
+    public static final Spell emptySpell = registerSpell(new EmptySpell());
     // SecondSpell
-    public static final Spell secondSpell = registerSpell(new MagicMissile());
+    public static final Spell magicMissile = registerSpell(new MagicMissile());
 
     private static Spell registerSpell(Spell spell) {
         return new SpellRegisterer().register(spell);
