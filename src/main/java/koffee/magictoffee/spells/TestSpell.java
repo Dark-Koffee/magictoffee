10ea764b47fd01ec37b8a638cd5b7c94d05b666b
kaiwoot <kaiwon.sacks@gmail.com>
2024-06-19 21:57:39 -0700

    changed user to player

--- src/main/java/koffee/magictoffee/spells/TestSpell.java
+++ src/main/java/koffee/magictoffee/spells/TestSpell.java
@@ -11,11 +11,11 @@ public class TestSpell extends Spell {
     }
 
     @Override
-    public void ActionOnUse(PlayerEntity user){
-        user.sendMessage(Text.literal("-- Spells --"), false);
-        user.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_BREAK, 2.0F, 1.0F);
+    public void ActionOnUse(PlayerEntity player){
+        player.sendMessage(Text.literal("-- Spells --"), false);
+        player.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_BREAK, 2.0F, 1.0F);
         for(int i = 0; i < SpellRegisterer.spells.size(); i++) {
-            user.sendMessage(Text.of(SpellRegisterer.spells.get(i).getID()), false);
+            player.sendMessage(Text.of(SpellRegisterer.spells.get(i).getID()), false);
         }
     }
 
