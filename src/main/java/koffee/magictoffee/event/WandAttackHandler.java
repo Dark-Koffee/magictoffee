8d1258650a2171d93bf60e65bf9c0c8a5f468e4b
RedToffee <Dreadnaxela@gmail.com>
2024-06-21 22:55:36 -0600

    Added cooldown logic

--- src/main/java/koffee/magictoffee/event/WandAttackHandler.java
+++ src/main/java/koffee/magictoffee/event/WandAttackHandler.java
@@ -1,10 +1,14 @@
 package koffee.magictoffee.event;
-import koffee.magictoffee.spells.Spell;
 import koffee.magictoffee.components.MagicComponent;
+import koffee.magictoffee.components.ModComponents;
+import koffee.magictoffee.spells.Spell;
 import koffee.magictoffee.util.SpellData;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import koffee.magictoffee.item.custom.WandItem;
@@ -22,9 +26,29 @@ public class WandAttackHandler {
                 if (isWandInHand(player, hand)) {
                     // Get the player's selected spell
                     Spell spell = SpellData.getSpellFromID(SpellData.getSpell(player, SpellData.getSelected(player)));
-                    // Run the player's selected spell's action
-                    spell.ActionOnUse(player);
-                    return TypedActionResult.success(player.getStackInHand(hand));
+                    String spellID = spell.getID();
+
+                    // Gets the player's magic component & spell cooldown, and current time
+                    MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+                    long currentTime = player.getWorld().getTime();
+                    long lastUsed = magicComponent.getCooldown(spellID);
+
+                    // Gets the spell's cooldown
+                    long spellCooldown = spell.getCooldown();
+
+                    if (currentTime >= lastUsed+spellCooldown) {
+                        // Cooldown is run before the action in case for some reason
+                        // you wanted to override the cooldown in your spell (e.g. in case of a failed spell)
+                        magicComponent.setCooldown(spellID, currentTime);
+                        // Run the player's selected spell's action
+                        if (spell.ActionOnUse(player)) {
+                            // Only return success if the action returns true
+                            return TypedActionResult.success(player.getStackInHand(hand));
+                        }
+                    } else {
+                        player.playSound(SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.AMBIENT, 0.5F, 1.0F);
+                        player.sendMessage(Text.literal("§cCooldown: " + String.format("%.2f", (lastUsed + spellCooldown - currentTime)/20.0f) + " Sec"), true);
+                    }
                 }
             }
             return TypedActionResult.pass(player.getStackInHand(hand));
