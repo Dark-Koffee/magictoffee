7bb49ebec05226c039ab4c3df8a9cdf29718095e
RedToffee <Dreadnaxela@gmail.com>
2024-06-21 21:30:20 -0600

    Updated already existing functions to work with new Components rather than the old Player Data

--- src/main/java/koffee/magictoffee/util/SpellData.java
+++ src/main/java/koffee/magictoffee/util/SpellData.java
@@ -1,79 +1,50 @@
 package koffee.magictoffee.util;
+import koffee.magictoffee.components.MagicComponent;
+import koffee.magictoffee.components.ModComponents;
 import koffee.magictoffee.spells.ModSpells;
 import koffee.magictoffee.spells.Spell;
 import koffee.magictoffee.spells.SpellRegisterer;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class SpellData {
-    public static int addSelected(IEntityDataSaver player, int amount) {
-        NbtCompound nbt = player.getPersistentData();
-        // Selected is set to 0 if it doesn't exist in the player's nbt
+    public static int addSelected(PlayerEntity player, int amount) {
+        MagicComponent component = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+        int selected = component.getSelected();
+        component.setSelected(selected+amount);
+        return selected+amount;
+    }
+    public static int getSelected(PlayerEntity player) {
+        MagicComponent component = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+        // Selected is set to 0 if it doesn't exist
         int selected = 0;
-        if (nbt.contains("selected", NbtElement.INT_TYPE)) {
-            selected = nbt.getInt("selected");
-        }
-
-        selected += amount;
-        if (selected > 4) {
-            selected = 0;
-        } else if (selected < 0) {
-            selected = 4;
-        }
-        nbt.putInt("selected", selected);
+        selected = component.getSelected();
         return selected;
     }
-    public static int getSelected(IEntityDataSaver player) {
-        NbtCompound nbt = player.getPersistentData();
-        // Selected is set to 0 if it doesn't exist in the player's nbt
-        int selected = 0;
-        if (nbt.contains("selected", NbtElement.INT_TYPE)) {
-            selected = nbt.getInt("selected");
-        }
-        return selected;
-    }
-    public static String setSpell(IEntityDataSaver player, int spellIndex, String spellName) {
-        NbtCompound nbt = player.getPersistentData();
-
-        // Define the keys for spell slots
-        String[] spellKeys = {"spell0", "spell1", "spell2", "spell3", "spell4"};
+    public static void setSpell(PlayerEntity player, int spellIndex, String spellName) {
+        MagicComponent component = ModComponents.SPELLS_COMPONENT_KEY.get(player);
 
         // Validate spellIndex to be within bounds
-        if (spellIndex < 0 || spellIndex >= spellKeys.length) {
+        if (spellIndex < 0 || spellIndex >= 5) {
             throw new IllegalArgumentException("Spell index out of bounds");
-        }
-
-        // Get the current value of the spell slot from NBT, defaulting to null if it doesn't exist
-        String currentSpell = nbt.getString(spellKeys[spellIndex]);
-
-        // Set default value to an empty string if the current value is null (not set)
-        if (currentSpell.isEmpty()) {
-            currentSpell = spellName; // Set to the new spell name
         }
 
         // Update the spell slot in NBT with the new spell name
-        nbt.putString(spellKeys[spellIndex], spellName);
-
-        // Return the updated spell name
-        return spellName;
+        component.setSpell(spellIndex, spellName);
     }
 
-    public static String getSpell(IEntityDataSaver player, int spellIndex) {
-        NbtCompound nbt = player.getPersistentData();
-
-        // Define the keys for spell slots
-        String[] spellKeys = {"spell0", "spell1", "spell2", "spell3", "spell4"};
+    public static String getSpell(PlayerEntity player, int spellIndex) {
+        MagicComponent component = ModComponents.SPELLS_COMPONENT_KEY.get(player);
 
         // Validate spellIndex to be within bounds
-        if (spellIndex < 0 || spellIndex >= spellKeys.length) {
+        if (spellIndex < 0 || spellIndex >= 5) {
             throw new IllegalArgumentException("Spell index out of bounds");
         }
 
         // Get the current value of the spell slot from NBT, defaulting to null if it doesn't exist
-        String currentSpell = nbt.getString(spellKeys[spellIndex]);
+        String currentSpell = component.getSpell(spellIndex);
 
         // Set default value to an empty spell if the current value is null
-        if (currentSpell.isEmpty()) {
+        if (currentSpell == null) {
             currentSpell = "magictoffee:empty";
         }
 
