8f20a0e58ecc653e4019ea8270f5150026e02fee
RedToffee <Dreadnaxela@gmail.com>
2024-06-21 22:53:38 -0600

    Changed Spell Actions from void to boolean

--- src/main/java/koffee/magictoffee/spells/EmptySpell.java
+++ src/main/java/koffee/magictoffee/spells/EmptySpell.java
@@ -7,11 +7,14 @@ public class EmptySpell extends Spell {
     EmptySpell() {
         super.spellID = "magictoffee:empty";
         super.displayName = "Empty Slot";
+        super.cooldown = 0;
+        super.manaCost = 0;
     }
 
     @Override
-    public void ActionOnUse(PlayerEntity user){
+    public boolean ActionOnUse(PlayerEntity user){
         user.sendMessage(Text.literal("\u00A77No spell selected"), true);
+        return false;
     }
 
 }
--- src/main/java/koffee/magictoffee/spells/TestSpell.java
+++ src/main/java/koffee/magictoffee/spells/TestSpell.java
@@ -9,15 +9,18 @@ public class TestSpell extends Spell {
     TestSpell() {
         super.spellID = "magictoffee:test";
         super.displayName = "Test Spell";
+        super.cooldown = 40;
+        super.manaCost = 0;
     }
 
     @Override
-    public void ActionOnUse(PlayerEntity player){
+    public boolean ActionOnUse(PlayerEntity player){
         player.sendMessage(Text.literal("-- Spells --"), false);
         player.playSound(SoundEvents.BLOCK_AMETHYST_CLUSTER_BREAK, 2.0F, 1.0F);
         for(int i = 0; i < SpellRegisterer.spells.size(); i++) {
             player.sendMessage(Text.of(SpellRegisterer.spells.get(i).getID()), false);
         }
+        return true;
     }
 
 }
