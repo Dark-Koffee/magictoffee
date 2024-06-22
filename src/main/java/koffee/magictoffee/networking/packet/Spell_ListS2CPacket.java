8e2d8b06f92cf02264982e427081f7ff1582cf23
RedToffee <Dreadnaxela@gmail.com>
2024-06-22 13:20:13 -0600

    Added Cooldowns and Comments

--- src/main/java/koffee/magictoffee/networking/packet/Spell_ListS2CPacket.java
+++ src/main/java/koffee/magictoffee/networking/packet/Spell_ListS2CPacket.java
@@ -1,5 +1,7 @@
 package koffee.magictoffee.networking.packet;
 
+import koffee.magictoffee.components.MagicComponent;
+import koffee.magictoffee.components.ModComponents;
 import koffee.magictoffee.networking.ModMessages;
 import koffee.magictoffee.util.SpellData;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
@@ -9,11 +11,19 @@ import net.minecraft.server.network.ServerPlayerEntity;
 
 public class Spell_ListS2CPacket {
     public static void send(ServerPlayerEntity player) {
+        // Create buf
         PacketByteBuf buf = PacketByteBufs.create();
+        // Write spells slots
         for (int i = 0; i < 5; i++) {
             buf.writeString(SpellData.getSpell(player, i), 32767);
         }
+        // Write selected slot
         buf.writeInt(SpellData.getSelected(player));
+        // Get player's magic component
+        MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+        // Write spell cooldowns
+        magicComponent.writeCooldownsToBuf(buf);
+        // Send the packet
         ServerPlayNetworking.send(player, ModMessages.SPELL_LIST, buf);
     }
 }
