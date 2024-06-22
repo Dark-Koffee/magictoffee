77552188506cdc9cf0c9ed1cd7910782c4902875
RedToffee <Dreadnaxela@gmail.com>
2024-06-22 13:14:00 -0600

    Accepted cooldowns from Spell_List Packet

--- src/client/java/koffee/magictoffee/client/ClientPacketHandler.java
+++ src/client/java/koffee/magictoffee/client/ClientPacketHandler.java
@@ -3,19 +3,31 @@ package koffee.magictoffee.client;
 import koffee.magictoffee.networking.ModMessages;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.network.ClientPlayerEntity;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientPacketHandler {
     public static String[] spells = new String[5];
     public static int selectedSpell;
+    public static final Map<String, Long> spellCooldowns = new HashMap<>();
     public static void register() {
         ClientPlayNetworking.registerGlobalReceiver(ModMessages.SPELL_LIST, (client, handler, buf, responseSender) -> {
             client.execute(() -> {
                 ClientPlayerEntity player = client.player;
+                // Register spells
                 for (int i = 0; i < 5; i++) {
                     // 32767 is the maximum string length in Minecraft
                     spells[i] = buf.readString(32767);
                 }
+                // Register selected
                 selectedSpell = buf.readInt();
+                // Register cooldowns
+                int size = buf.readInt(); // Read the size of the map
+                for (int i = 0; i < size; i++) {
+                    String spellId = buf.readString(); // Read the spell ID
+                    long cooldown = buf.readLong(); // Read the cooldown
+                    spellCooldowns.put(spellId, cooldown); // Put the data into the map
+                }
             });
         });
     }
