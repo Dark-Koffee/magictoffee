f5d7eb1a781e6f9f1a071513c89ad08dec5a6a45
RedToffee <Dreadnaxela@gmail.com>
2024-06-22 22:27:32 -0600

    Put reused code in functions

--- src/main/java/koffee/magictoffee/spells/MagicMissile.java
+++ src/main/java/koffee/magictoffee/spells/MagicMissile.java
@@ -1,5 +1,6 @@
 package koffee.magictoffee.spells;
 
+import koffee.magictoffee.components.MagicComponent;
 import koffee.magictoffee.components.ModComponents;
 import koffee.magictoffee.util.KoffeeSpellTools;
 import koffee.magictoffee.util.Particles;
@@ -42,81 +43,69 @@ public class MagicMissile extends Spell {
         // "Wait a second toffee, that looks like a lot of reused code why don't you just use funct-" Shhhhhhh
         // To those people I say, If it ain't broke, don't fix it. ~~ Toffee 2024
 
+        // Fine I caved in and function-ed it. BUT IT WAS BEGRUDGINGLY
 
         // If it doesn't hit anything
         if (targetEntity == null && targetBlock == null) {
             Particles.drawCircle(world, eyePos, 0.5, 0, 0, 32, ParticleTypes.INSTANT_EFFECT, 0);
             player.playSound(SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.AMBIENT, 1.0F, 1.0F);
-            ModComponents.SPELLS_COMPONENT_KEY.get(player).setCooldown(spellID, 0);
+            MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+            magicComponent.setCooldown(spellID, player.getWorld().getTime() - cooldown/2);
+            magicComponent.setMana(magicComponent.getMana()+manaCost);
             return false;
+        // If it hit something
         } else {
-            // If only the block is valid
+            // If only the block is valid - hit block
             if (targetEntity == null) {
-                // Move line down 0.2 blocks
-                eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
-                Particles.drawLine(world, eyePos, player.raycast(20.0D, 1.0F, true).getPos(), 30, ParticleTypes.ELECTRIC_SPARK, 0);
-                player.playSound(SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.AMBIENT, 1.0F, 1.0F);
-                ModComponents.SPELLS_COMPONENT_KEY.get(player).setCooldown(spellID, player.getWorld().getTime()-5L);
-                return false;
+                return hitBlockAction(eyePos, player, world);
             }
-            // If only the entity is valid
+            // If only the entity is valid - hit entity
             else if (targetBlock == null) {
-                // If it's a living entity
-                if (targetEntity instanceof LivingEntity) {
-                    targetEntityLoc = targetEntity.getEyePos();
-                    // Move line down 0.2 blocks
-                    targetEntityLoc = new Vec3d(targetEntityLoc.x, targetEntityLoc.y - 0.2, targetEntityLoc.z);
-//                ((LivingEntity) entity).setHealth(((LivingEntity) entity).getHealth()-1);
-                    targetEntity.damage(targetEntity.getDamageSources().generic(), 2.0F);
-                }
-
-                // pushes the target entity backwards
-                Vec3d velocity = (targetEntity.getPos()).subtract(player.getPos());
-                Vec3d desiredVelocity = (velocity.normalize()).multiply(0.5);
-                targetEntity.addVelocity(desiredVelocity.x, 0.5, desiredVelocity.z);
-
-                // Move line down 0.2 blocks
-                eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
-
-                // Draw particle line
-                Particles.drawLine(world, eyePos, targetEntityLoc, 30, ParticleTypes.ELECTRIC_SPARK, 0);
-                player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_HIT, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                return true;
+                return hitEntityAction(targetEntity, targetEntityLoc, player, eyePos, world);
             }
             // Neither is null
-            // If the target entity is closer
+            // If the target entity is closer - hit entity
             else if (targetEntityLoc.distanceTo(eyePos) < targetBlock.distanceTo(eyePos)) {
-                // If it's a living entity
-                if (targetEntity instanceof LivingEntity) {
-                    targetEntityLoc = targetEntity.getEyePos();
-                    // Move line down 0.2 blocks
-                    targetEntityLoc = new Vec3d(targetEntityLoc.x, targetEntityLoc.y - 0.2, targetEntityLoc.z);
-//                ((LivingEntity) entity).setHealth(((LivingEntity) entity).getHealth()-1);
-                    targetEntity.damage(targetEntity.getDamageSources().generic(), 2.0F);
-                }
-
-                // pushes the target entity backwards
-                Vec3d velocity = (targetEntity.getPos()).subtract(player.getPos());
-                Vec3d desiredVelocity = (velocity.normalize()).multiply(0.5);
-                targetEntity.addVelocity(desiredVelocity.x, 0.5, desiredVelocity.z);
-
-                // Move line down 0.2 blocks
-                eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
-
-                // Draw particle line
-                Particles.drawLine(world, eyePos, targetEntityLoc, 30, ParticleTypes.ELECTRIC_SPARK, 0);
-                player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_HIT, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                return true;
+                return hitEntityAction(targetEntity, targetEntityLoc, player, eyePos, world);
             }
-            // If the target block is closer
+            // If the target block is closer - hit block
             else {
-                // Move line down 0.2 blocks
-                eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
-                Particles.drawLine(world, eyePos, player.raycast(20.0D, 1.0F, true).getPos(), 30, ParticleTypes.ELECTRIC_SPARK, 0);
-                player.playSound(SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.AMBIENT, 1.0F, 1.0F);
-                ModComponents.SPELLS_COMPONENT_KEY.get(player).setCooldown(spellID, player.getWorld().getTime()-5L);
-                return false;
+                return hitBlockAction(eyePos, player, world);
             }
         }
+    }
+
+    private boolean hitEntityAction(Entity targetEntity, Vec3d targetEntityLoc, PlayerEntity player, Vec3d eyePos, ServerWorld world) {
+        // If it's a living entity
+        if (targetEntity instanceof LivingEntity) {
+            targetEntityLoc = targetEntity.getEyePos();
+            // Move line down 0.2 blocks
+            targetEntityLoc = new Vec3d(targetEntityLoc.x, targetEntityLoc.y - 0.2, targetEntityLoc.z);
+//                ((LivingEntity) entity).setHealth(((LivingEntity) entity).getHealth()-1);
+            targetEntity.damage(targetEntity.getDamageSources().generic(), 2.0F);
+        }
+
+        // pushes the target entity backwards
+        Vec3d velocity = (targetEntity.getPos()).subtract(player.getPos());
+        Vec3d desiredVelocity = (velocity.normalize()).multiply(0.5);
+        targetEntity.addVelocity(desiredVelocity.x, 0.5, desiredVelocity.z);
+
+        // Move line down 0.2 blocks
+        eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
+
+        // Draw particle line
+        Particles.drawLine(world, eyePos, targetEntityLoc, 30, ParticleTypes.ELECTRIC_SPARK, 0);
+        player.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_HIT, SoundCategory.BLOCKS, 1.0F, 1.0F);
+        return true;
+    }
+    private boolean hitBlockAction(Vec3d eyePos, PlayerEntity player, ServerWorld world) {
+        // Move line down 0.2 blocks
+        eyePos = new Vec3d(eyePos.x, eyePos.y - 0.2, eyePos.z);
+        Particles.drawLine(world, eyePos, player.raycast(20.0D, 1.0F, true).getPos(), 30, ParticleTypes.ELECTRIC_SPARK, 0);
+        player.playSound(SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.AMBIENT, 1.0F, 1.0F);
+        MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+        magicComponent.setCooldown(spellID, player.getWorld().getTime() - cooldown/2);
+        magicComponent.setMana(magicComponent.getMana()+manaCost);
+        return false;
     }
 }
