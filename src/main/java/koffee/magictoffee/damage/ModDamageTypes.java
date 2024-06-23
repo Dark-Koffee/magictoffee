fe6b92b0c1c31c1356bc58e85e3878f02b3238ac
RedToffee <Dreadnaxela@gmail.com>
2024-06-23 00:19:43 -0600

    Made custom damage type

--- src/main/java/koffee/magictoffee/damage/ModDamageTypes.java
+++ src/main/java/koffee/magictoffee/damage/ModDamageTypes.java
@@ -1,13 +1,19 @@
 package koffee.magictoffee.damage;
 
 import koffee.magictoffee.MagicToffee;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageType;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 public class ModDamageTypes {
 
-    public static final RegistryKey<DamageType> MAGICMISSILE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier(MagicToffee.MOD_ID));
+    public static final RegistryKey<DamageType> MAGICMISSILE = RegistryKey.of(RegistryKeys.DAMAGE_TYPE, new Identifier(MagicToffee.MOD_ID, "magicmissile"));
 
+    public static DamageSource of(World world, RegistryKey<DamageType> key, Entity attacker, Entity victim) {
+        return new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).entryOf(key), attacker, victim);
+    }
 }
