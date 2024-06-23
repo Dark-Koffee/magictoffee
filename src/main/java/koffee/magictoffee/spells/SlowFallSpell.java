c8b42517944199165a8d0fdc4d616531a1ff1def
RedToffee <Dreadnaxela@gmail.com>
2024-06-23 17:51:30 -0600

    Added velocity change

--- src/main/java/koffee/magictoffee/spells/SlowFallSpell.java
+++ src/main/java/koffee/magictoffee/spells/SlowFallSpell.java
@@ -6,6 +6,7 @@ import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.Vec3d;
 
 public class SlowFallSpell extends Spell {
 
@@ -19,9 +20,16 @@ public class SlowFallSpell extends Spell {
     @Override
     public boolean ActionOnUse(PlayerEntity player){
         ServerWorld world = (ServerWorld) player.getWorld();
+        // Spawn cloud particles
         world.spawnParticles(ParticleTypes.CLOUD, player.getX(), player.getY(), player.getZ(), 30, 0, 0, 0, 0.1);
+        // Effect player w/ slow falling
         player.setStatusEffect(new StatusEffectInstance(StatusEffects.SLOW_FALLING, 40, 0), player);
+        // Play flap sound
         player.playSound(SoundEvents.ENTITY_PHANTOM_FLAP, SoundCategory.AMBIENT, 1F, 1F);
+        // Change player's velocity
+        Vec3d playerVel = player.getVelocity();
+        player.setVelocity(new Vec3d(playerVel.x, 0.1, playerVel.z));
+        player.velocityModified = true;
         return true;
     }
 
