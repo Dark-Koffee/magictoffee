b4f46f73e9a14baec2fec42e29787e1ee3443f55
RedToffee <Dreadnaxela@gmail.com>
2024-06-22 22:26:50 -0600

    Cleaned code

--- src/client/java/koffee/magictoffee/client/ClientPacketHandler.java
+++ src/client/java/koffee/magictoffee/client/ClientPacketHandler.java
@@ -11,25 +11,23 @@ public class ClientPacketHandler {
     public static final Map<String, Long> spellCooldowns = new HashMap<>();
     public static int mana = 100;
     public static void register() {
-        ClientPlayNetworking.registerGlobalReceiver(ModMessages.SPELL_LIST, (client, handler, buf, responseSender) -> {
-            client.execute(() -> {
-                // Register spells
-                for (int i = 0; i < 5; i++) {
-                    // 32767 is the maximum string length in Minecraft
-                    spells[i] = buf.readString(32767);
-                }
-                // Register selected
-                selectedSpell = buf.readInt();
-                // Register mana
-                mana = buf.readInt();
-                // Register cooldowns
-                int size = buf.readInt(); // Read the size of the map
-                for (int i = 0; i < size; i++) {
-                    String spellId = buf.readString(); // Read the spell ID
-                    long cooldown = buf.readLong(); // Read the cooldown
-                    spellCooldowns.put(spellId, cooldown); // Put the data into the map
-                }
-            });
-        });
+        ClientPlayNetworking.registerGlobalReceiver(ModMessages.SPELL_LIST, (client, handler, buf, responseSender) -> client.execute(() -> {
+            // Register spells
+            for (int i = 0; i < 5; i++) {
+                // 32767 is the maximum string length in Minecraft
+                spells[i] = buf.readString(32767);
+            }
+            // Register selected
+            selectedSpell = buf.readInt();
+            // Register mana
+            mana = buf.readInt();
+            // Register cooldowns
+            int size = buf.readInt(); // Read the size of the map
+            for (int i = 0; i < size; i++) {
+                String spellId = buf.readString(); // Read the spell ID
+                long cooldown = buf.readLong(); // Read the cooldown
+                spellCooldowns.put(spellId, cooldown); // Put the data into the map
+            }
+        }));
     }
 }
