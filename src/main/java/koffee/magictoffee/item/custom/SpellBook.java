012d9fc9888fc96272c6be7dcab1bfef163b4dbd
RedToffee <Dreadnaxela@gmail.com>
2024-06-23 23:02:09 -0600

    Added tooltip to spellbooks

--- src/main/java/koffee/magictoffee/item/custom/SpellBook.java
+++ src/main/java/koffee/magictoffee/item/custom/SpellBook.java
@@ -1,8 +1,16 @@
 package koffee.magictoffee.item.custom;
 
 import koffee.magictoffee.spells.Spell;
+import koffee.magictoffee.util.SpellData;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SpellBook extends Item {
     public SpellBook(Settings settings) {
@@ -10,14 +18,34 @@ public class SpellBook extends Item {
     }
 
     // Method to set the spell
-    public static void setSpell(ItemStack stack, Spell spell) {
+    public static void setSpell(ItemStack stack, String spellID) {
         NbtCompound nbt = stack.getOrCreateNbt();
-        nbt.putString("Spell", spell.getID());
+        nbt.putString("Spell", spellID);
     }
 
     // Method to get the spell
     public static String getSpell(ItemStack stack) {
         NbtCompound nbt = stack.getNbt();
         return nbt != null ? nbt.getString("Spell"): null;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        String spellID = getSpell(stack);
+        // Default spell
+        Spell spell = new Spell() {
+            @Override
+            public boolean ActionOnUse(PlayerEntity player) {
+                return false;
+            }
+        };
+        if (spellID != null) {
+            spell = SpellData.getSpellFromID(spellID);
+        }
+        tooltip.add(Text.literal("§7" + spell.getBookName()));
+        tooltip.add(Text.literal("§8" + spell.getDescription()));
+        tooltip.add(Text.literal("§9Mana Cost: " + spell.getManaCost()));
+        tooltip.add(Text.literal("§9Cooldown: §9" + String.format("%.1f", (float) spell.getCooldown()/20F) + " seconds"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
