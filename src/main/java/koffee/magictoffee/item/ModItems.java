7103ae2e2907613c3b470fa30e341d5ecda7f565
RedToffee <Dreadnaxela@gmail.com>
2024-06-23 23:00:48 -0600

    Made registerItem public

--- src/main/java/koffee/magictoffee/item/ModItems.java
+++ src/main/java/koffee/magictoffee/item/ModItems.java
@@ -4,10 +4,7 @@ import koffee.magictoffee.MagicToffee;
 import koffee.magictoffee.item.custom.SpellBook;
 import koffee.magictoffee.item.custom.WandItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -17,7 +14,7 @@ public class ModItems {
 
     public static final Item spellbook = registerItem("spellbook", new SpellBook(new FabricItemSettings().maxCount(1)));
 
-    private static Item registerItem(String name, Item item) {
+    public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MagicToffee.MOD_ID, name), item);
     }
 
