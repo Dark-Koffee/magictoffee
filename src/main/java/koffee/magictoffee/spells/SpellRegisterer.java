e4cd53df5cb61d170213e651ae40d35d3837dfa2
RedToffee <Dreadnaxela@gmail.com>
2024-06-23 23:02:53 -0600

    Added Spellbooks to Magic Toffee item group

--- src/main/java/koffee/magictoffee/spells/SpellRegisterer.java
+++ src/main/java/koffee/magictoffee/spells/SpellRegisterer.java
@@ -1,5 +1,9 @@
 package koffee.magictoffee.spells;
 
+import koffee.magictoffee.item.ModItems;
+import koffee.magictoffee.item.custom.SpellBook;
+import net.minecraft.item.ItemStack;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +11,14 @@ public class SpellRegisterer {
     public static List<Spell> spells = new ArrayList<>();
 
     public Spell register(Spell spell) {
+        // Add spell to spell list
         spells.add(spell);
+        // Make spell book
+        if (spell.registerBook) {
+            ItemStack stack = new ItemStack(ModItems.spellbook);
+            SpellBook.setSpell(stack, spell.getID());
+        }
+        // Return
         return spell;
     }
 
@@ -17,10 +28,6 @@ public class SpellRegisterer {
             spellNames[i] = spells.get(i).getID();
         }
         return spellNames;
-    }
-
-    public void registerSpellBook(Spell spell) {
-
     }
 }
 
