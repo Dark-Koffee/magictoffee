98de05ec567e29f8dd1569c4a531a40709109f57
RedToffee <Dreadnaxela@gmail.com>
2024-06-25 15:07:13 -0600

    Registered Heart Particle

--- src/client/java/koffee/magictoffee/MagicToffeeClient.java
+++ src/client/java/koffee/magictoffee/MagicToffeeClient.java
@@ -4,12 +4,13 @@ import koffee.magictoffee.HudOverlay.SpellsListHud;
 import koffee.magictoffee.block.ModBlocks;
 import koffee.magictoffee.client.ClientPacketHandler;
 import koffee.magictoffee.event.LeftClickHandler;
-import koffee.magictoffee.particles.ParticleFactory;
 import koffee.magictoffee.screen.SpellcasterScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.particle.EmotionParticle;
 import net.minecraft.client.render.RenderLayer;
 
 public class MagicToffeeClient implements ClientModInitializer {
@@ -30,6 +31,6 @@ public class MagicToffeeClient implements ClientModInitializer {
 		HandledScreens.register(MagicToffee.SPELLCASTER_SCREEN_HANDLER_SCREEN_HANDLER_TYPE, SpellcasterScreen::new);
 
 		// Register particles
-		ParticleFactory.registerParticleFactories();
+		ParticleFactoryRegistry.getInstance().register(MagicToffee.HEART, EmotionParticle.HeartFactory::new);
 	}
 }
