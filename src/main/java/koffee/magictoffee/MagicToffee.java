c150a06400ea1c4f61e81c507d1ed4e4b421e051
RedToffee <Dreadnaxela@gmail.com>
2024-07-01 17:39:24 -0600

    Changed method of Mana Regeneration
    Known bug: Mana still regenerates when game is paused

--- src/main/java/koffee/magictoffee/MagicToffee.java
+++ src/main/java/koffee/magictoffee/MagicToffee.java
@@ -16,7 +16,6 @@ import koffee.magictoffee.screen.SpellcasterScreenHandler;
 import koffee.magictoffee.spells.ModSpells;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
@@ -28,6 +27,13 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class MagicToffee implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -49,6 +55,11 @@ public class MagicToffee implements ModInitializer {
 				new Identifier(MOD_ID, "spellcaster"), SpellcasterScreenHandler::new
 		);
 	}
+
+	// manaRegen variables
+	private static final int TICKS_PER_SECOND = 20;
+	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	private static final Map<ServerPlayerEntity, ScheduledFuture<?>> playerTasks = new HashMap<>();
 
 
 	@Override
@@ -80,13 +91,6 @@ public class MagicToffee implements ModInitializer {
 		// Particles
 		Registry.register(Registries.PARTICLE_TYPE, new Identifier(MOD_ID, "heart"), HEART);
 
-		// Register the event listener for player join
-		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-			ServerPlayerEntity player = handler.getPlayer();
-			Spell_ListS2CPacket.send(player);
-			LOGGER.info(player.getName().getString() + " successfully sent Spell Packets on Join");
-		});
-
 		// Commands
 		CommandRegistrationCallback.EVENT.register(SpellsCommand::register);
 		CommandRegistrationCallback.EVENT.register(ManaCommand::register);
@@ -94,20 +98,52 @@ public class MagicToffee implements ModInitializer {
 		// Components
 		ModComponents.register();
 
-		// Timer
-		ServerTickEvents.END_SERVER_TICK.register(world -> {
-			tickCounter++;
-			if (tickCounter >= 20) {
-				tickCounter = 0;
-				for (ServerPlayerEntity player : world.getPlayerManager().getPlayerList()) {
-					MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+		// Register the event listener for player join
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			ServerPlayerEntity player = handler.getPlayer();
+			Spell_ListS2CPacket.send(player);
+			LOGGER.info(player.getName().getString() + " successfully sent Spell Packets on Join");
+
+			// Register mana regen
+			scheduleManaRegenTask(player);
+		});
+
+		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+			ServerPlayerEntity player = handler.getPlayer();
+			cancelManaRegenTask(player);
+		});
+	}
+
+	private static void scheduleManaRegenTask(ServerPlayerEntity player) {
+		MagicComponent magicComponent = ModComponents.SPELLS_COMPONENT_KEY.get(player);
+		int manaRegen = magicComponent.getManaRegen();
+		if (manaRegen > 0) {
+			long delay = 1000 / manaRegen; // Calculate delay based on manaRegen
+			Runnable task = () -> {
+				if (player.isAlive()) {
 					int mana = magicComponent.getMana();
-					if (mana < 100) {
+					int manaCap = magicComponent.getManaCap();
+					if (mana < manaCap) {
 						magicComponent.setMana(mana + 1);
 						Spell_ListS2CPacket.send(player);
 					}
 				}
-			}
-		});
+			};
+
+			ScheduledFuture<?> scheduledTask = scheduler.scheduleAtFixedRate(task, delay, delay, TimeUnit.MILLISECONDS);
+			playerTasks.put(player, scheduledTask);
+		}
+	}
+
+	private static void cancelManaRegenTask(ServerPlayerEntity player) {
+		ScheduledFuture<?> scheduledTask = playerTasks.remove(player);
+		if (scheduledTask != null) {
+			scheduledTask.cancel(true);
+		}
+	}
+
+	public static void refreshManaRegenTask(ServerPlayerEntity player) {
+		cancelManaRegenTask(player);
+		scheduleManaRegenTask(player);
 	}
 }
