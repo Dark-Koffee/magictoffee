599d92e41163d752ec8b996e216f9f8cef683d1b
RedToffee <Dreadnaxela@gmail.com>
2024-07-01 17:36:15 -0600

    Registered mana expander, catalyst, and flasks

--- src/main/java/koffee/magictoffee/item/ModItems.java
+++ src/main/java/koffee/magictoffee/item/ModItems.java
@@ -1,6 +1,8 @@
 package koffee.magictoffee.item;
 
 import koffee.magictoffee.MagicToffee;
+import koffee.magictoffee.item.custom.ManaFlaskItem;
+import koffee.magictoffee.item.custom.ManaModifier;
 import koffee.magictoffee.item.custom.SpellBook;
 import koffee.magictoffee.item.custom.WandItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -13,6 +15,14 @@ public class ModItems {
     public static final Item wand = registerItem("wand", new WandItem(new FabricItemSettings().maxCount(1)));
 
     public static final Item spellbook = registerItem("spellbook", new SpellBook(new FabricItemSettings().maxCount(1)));
+
+    public static final Item mana_expander = registerItem("mana_expander", new ManaModifier(new FabricItemSettings().maxCount(1)));
+
+    public static final Item mana_catalyst = registerItem("mana_catalyst", new ManaModifier(new FabricItemSettings().maxCount(1)));
+
+    public static final Item mana_flask = registerItem("mana_flask", new ManaFlaskItem(new FabricItemSettings().maxCount(1)));
+
+    public static final Item glass_flask = registerItem("glass_flask", new Item(new FabricItemSettings()));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MagicToffee.MOD_ID, name), item);
